e4dfd0a3b29935672d1ed7b88a492e95aa643954
newmersedez <trishkk2001@gmail.com>
2021-11-09 22:34:12 +0300

    moved vertex and verge classes

--- classes/verge.py
+++ classes/verge.py
@@ -3,6 +3,19 @@ from utils.defines import *
 from classes.vertex import *
 
 class Verge:
-    def __init__(self, start_vertex: Vertex, end_vertex: Vertex):
+    def __init__(self, start_vertex: Vertex, end_vertex: Vertex, weight: int):
+        self.weight = weight
         self.startVertex = start_vertex
+        self.endVertex = end_vertex
+
+    def getStartVertex(self):
+        return self.startVertex
+
+    def getEndVertex(self):
+        return self.endVertex
+
+    def setStartVertex(self, start_vertex):
+        self.startVertex = start_vertex
+
+    def setEndVertex(self, end_vertex):
         self.endVertex = end_vertex
--- classes/vertex.py
+++ classes/vertex.py
@@ -1,5 +1,4 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
-from utils.defines import *
+from utils.defines import VERTEX_RADIUS
 
 
 class Vertex:
@@ -8,5 +7,65 @@ class Vertex:
         self.y = y
         self.name = name
         self.color = color
-        self.radius = VERTEX_RADIUS
         self.adjacentVertexList = list()
+        self.dragging = False
+
+    def getPos(self):
+        return self.x, self.y
+
+    def getName(self):
+        return self.name
+
+    def getColor(self):
+        return self.color
+
+    def getAdjacentVertexList(self):
+        return self.adjacentVertexList
+
+    def setPos(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def setName(self, name: str):
+        self.name = name
+
+    def setColor(self, color: str):
+        self.color = color
+
+    def addAdjacentVertex(self, vertex):
+        self.adjacentVertexList.append(vertex)
+
+    def removeAdjacentVertex(self, vertex):
+        if vertex in self.adjacentVertexList:
+            self.adjacentVertexList.remove(vertex)
+
+    def findAdjacentVertex(self, vertex):
+        if vertex in self.adjacentVertexList:
+            return True
+        return False
+
+    def draggingStatus(self):
+        return self.dragging
+
+    def draggingStart(self):
+        self.dragging = True
+
+    def draggingStop(self):
+        self.dragging = False
+
+    def collidePoint(self, x: int, y: int):
+        dist_x = abs(self.x - x)
+        dist_y = abs(self.y - y)
+
+        if (dist_x <= VERTEX_RADIUS) and (dist_y <= VERTEX_RADIUS):
+            return True
+        return False
+
+    def collide_vertex(self, vertex):
+        center_x, center_y = vertex.getPos()
+        dist_x = abs(self.x - center_x)
+        dist_y = abs(self.y - center_y)
+
+        if (dist_x <= 2 * VERTEX_RADIUS) and (dist_y <= 2 * VERTEX_RADIUS):
+            return True
+        return False
