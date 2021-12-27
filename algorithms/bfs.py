40c277529b24c93a8239ca3a9e62153359ed2b88
newmersedez <trishkk2001@gmail.com>
2021-12-27 21:29:32 +0300

    bfs fixed bug

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -5,11 +5,15 @@ from queue import Queue
 def bfs(graph: Graph, vertex: Vertex):
     visited_nodes = [False] * len(graph.getVertexList())
     shortest_ways = {}
+    depth = {}
     bfs_steps = []
 
     for i in graph.getVertexList():
         shortest_ways[i] = None
+        depth[i] = None
     shortest_ways[vertex] = 0
+    depth[vertex] = 0
+
     nodes_to_visit = Queue()
     nodes_to_visit.put(vertex)
     while nodes_to_visit.qsize() > 0:
@@ -20,17 +24,15 @@ def bfs(graph: Graph, vertex: Vertex):
             range_vertex = new_vertex.rangeToAdjNode(i)
             if range_vertex is not None and shortest_ways[i] is None and new_vertex.isReachable(i):
                 shortest_ways[i] = shortest_ways[new_vertex] + range_vertex
+                depth[i] = depth[new_vertex] + 1
                 tmp.append(i)
                 nodes_to_visit.put(i)
         if len(tmp) > 0:
             bfs_steps.append(tmp)
 
-    for i in range(0, len(bfs_steps)):
-        for vertex in bfs_steps[i]:
-            vertex.setColor("red")
-            vertex.setServiceValue("n=" + str(i + 1))
+    for i in shortest_ways.keys():
+        if (depth[i] != 0):
+            i.setColor("red")
+            i.setServiceValue("n=" + str(depth[i]))
 
     return shortest_ways
-
-
-
